aca440ea2749546f3c9decc06522243853d685e9
Communios <70321048+Communios@users.noreply.github.com>
2021-07-25 16:24:39 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# An-Chrome-extension
-yes
+Remember:
+========
+
+#### This is just a beta version,  not included delete button.
+
+#### Your information cannot be deleted unless you turn off and delete the extension.
+**Left your computer while this exetension is not uninstalled is not an option.**
---
+++ script.js
@@ -0,0 +1,39 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+}
+
+inputBtn.addEventListener("click", function() {
+  myLeads.push(inputEl.value)
+  inputEl.value = ''
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  renderLeads()
+  console.log(localStorage.getItem("myLeads"))
+})
+
+
+
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+      <li>
+        <a target='_blank' href='${myLeads[i]}'>
+          ${myLeads[i]}
+        </a>
+      </li>
+    `
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+  }
+
+  ulEl.innerHTML = listItems
+}
